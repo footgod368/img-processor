51a716fd1bd088eeb38583223b22b6f994c98b5f
libole <libole@bytedance.com>
2025-12-13 02:16:32 +0800

    1

--- internal/mirror.go
+++ internal/mirror.go
@@ -1,12 +1,25 @@
 package internal
 
-import "image"
+import (
+	"image"
+	"image/color"
+)
 
 type mirrorImage struct {
 }
 
 func (mirrorImage) Process(img image.Image) (image.Image, error) {
-	return nil, nil
+	rect := img.Bounds()
+	newImg := image.NewRGBA(rect)
+	for y := rect.Min.Y; y < rect.Max.Y; y++ {
+		for left, right := rect.Min.X, rect.Max.X; left < right; left, right = left+1, right-1 {
+			rLeft, gLeft, bLeft, aLeft := img.At(left, y).RGBA()
+			rRight, gRight, bRight, aRight := img.At(right, y).RGBA()
+			newImg.Set(left, y, color.RGBA{uint8(rRight), uint8(gRight), uint8(bRight), uint8(aRight)})
+			newImg.Set(right, y, color.RGBA{uint8(rLeft), uint8(gLeft), uint8(bLeft), uint8(aLeft)})
+		}
+	}
+	return newImg, nil
 }
 
 func (mirrorImage) OutPrefix() string {
